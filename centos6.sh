232c0b47acef0b134cf4b6d7ec97428d7d468a3b
micky24 <micky.teguh@gmail.com>
2014-11-13 06:25:35 +0700

    VPS

--- centos6.sh
+++ centos6.sh
@@ -98,13 +98,13 @@ service php-fpm restart
 service nginx restart
 
 # install openvpn
-wget -O /etc/openvpn/openvpn.tar "https://raw.github.com/micky24/debian7/master/openvpn-debian.tar"
-cd /etc/openvpn/
-tar xf openvpn.tar
-wget -O /etc/openvpn/1194.conf "https://raw.github.com/micky24/centos6/master/vps.conf"
-if [ "$OS" == "x86_64" ]; then
-  wget -O /etc/openvpn/1194.conf "https://raw.github.com/micky24/centos6/master/1194-centos64.conf"
-fi
+#wget -O /etc/openvpn/openvpn.tar "https://raw.github.com/micky24/debian7/master/openvpn-debian.tar"
+#cd /etc/openvpn/
+#tar xf openvpn.tar
+#wget -O /etc/openvpn/1194.conf "https://raw.github.com/micky24/centos6/master/vps.conf"
+#if [ "$OS" == "x86_64" ]; then
+#  wget -O /etc/openvpn/1194.conf "https://raw.github.com/micky24/centos6/master/1194-centos64.conf"
+#fi
 wget -O /etc/iptables.up.rules "https://raw.github.com/micky24/centos6/master/iptables.up.rules"
 sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
 sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.d/rc.local
@@ -112,22 +112,22 @@ sed -i $MYIP2 /etc/iptables.up.rules;
 iptables-restore < /etc/iptables.up.rules
 sysctl -w net.ipv4.ip_forward=1
 sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
-service openvpn restart
-chkconfig openvpn on
+#service openvpn restart
+#chkconfig openvpn on
 cd
 
 # configure openvpn client config
-cd /etc/openvpn/
-wget -O /etc/openvpn/1194-client.ovpn "https://raw.github.com/micky24/centos6/master/1194-client.conf"
-sed -i $MYIP2 /etc/openvpn/1194-client.ovpn;
-PASS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1`;
-useradd -M -s /bin/false micky24
-echo "micky24:$PASS" | chpasswd
-echo "username" > pass.txt
-echo "password" >> pass.txt
-tar cf client.tar 1194-client.ovpn pass.txt
-cp client.tar /home/vps/public_html/
-cd
+#cd /etc/openvpn/
+#wget -O /etc/openvpn/1194-client.ovpn "https://raw.github.com/micky24/centos6/master/1194-client.conf"
+#sed -i $MYIP2 /etc/openvpn/1194-client.ovpn;
+#PASS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1`;
+#useradd -M -s /bin/false micky24
+#echo "micky24:$PASS" | chpasswd
+#echo "username" > pass.txt
+#echo "password" >> pass.txt
+#tar cf client.tar 1194-client.ovpn pass.txt
+#cp client.tar /home/vps/public_html/
+#cd
 
 # install badvpn
 wget -O /usr/bin/badvpn-udpgw "https://raw.github.com/micky24/centos6/master/badvpn-udpgw"
